900cedfde31edc2eacc76036f413c6cf1acf7c92
JamesDeChavez <jamesdechavez23@gmail.com>
2023-04-01 12:34:04 -0700

    updated shop to improve image rendering

--- client/src/components/ShopUnits/index.tsx
+++ client/src/components/ShopUnits/index.tsx
@@ -9,9 +9,11 @@ const ShopUnits = () => {
     const className = 'ShopUnits'
     return (
         <div className={className}>
-            {champShop.map((champ, i) => {
-                return <Unit champData={champ} key={i} index={i} />
-            })}        
+            <Unit champData={champShop[0]} key={`Unit_0`} index={0} />
+            <Unit champData={champShop[1]} key={`Unit_1`} index={1} />
+            <Unit champData={champShop[2]} key={`Unit_2`} index={2} />
+            <Unit champData={champShop[3]} key={`Unit_3`} index={3} />
+            <Unit champData={champShop[4]} key={`Unit_4`} index={4} />
         </div>
     )
 }
